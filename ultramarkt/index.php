ac1b10f727f10d6a9495f25cfe582aad4d6b3363
SanderBuruma <SanderBuruma@gmail.com>
2018-10-29 21:50:10 +0100

    commit before implementing search function

--- ultramarkt/index.php
+++ ultramarkt/index.php
@@ -4,7 +4,7 @@ $_SESSION['page'] = "home";
 require('includes/navbar.php');
 ?>
 <div class="wrapper">
-  <form action="./">
+  <form action="index.php">
     <p class="success"><?php echo $_SESSION['success']; $_SESSION['success'] = '' ?></p>
     <p class="warning"><?php echo $_SESSION['warning']; $_SESSION['warning'] = '' ?></p>
     <input type="text" name="searchInput" id="search-input" placeholder="typ hier uw zoekopdracht!">
