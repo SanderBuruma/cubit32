fc45713ce26cae134be08a8726eed46c2925a448
SanderBuruma <SanderBuruma@gmail.com>
2018-10-30 16:57:58 +0100

    search function now works, but needs to be styled

---
+++ ultramarkt/class.user.php
@@ -0,0 +1,13 @@
+<?php
+
+class User{
+
+  public $username;
+  public $userID;
+  protected $password;
+  public $sessionID;
+
+  function __constructor(){
+    
+  }
+}
--- ultramarkt/index.php
+++ ultramarkt/index.php
@@ -7,7 +7,7 @@ require_once('includes/get.categorieen.php');
 
 ?>
 <div class="wrapper">
-  <form action="index.php">
+  <form action="index.php" method="GET">
     <p class="success"><?php echo $_SESSION['success']; $_SESSION['success'] = '' ?></p>
     <p class="warning"><?php echo $_SESSION['warning']; $_SESSION['warning'] = '' ?></p>
     <div id="search-input">
@@ -25,23 +25,25 @@ require_once('includes/get.categorieen.php');
     </div>
   </form>
     <?php
-    if (isset($_POST['submit'])){
+    if (isset($_GET['categorieID'])){
       echo '<div id="zoek-resultaten">';
       
       function postAdvertenties($stmt){
         $stmt->execute();
-        $stmt->bind_result($advertentieID,$prijs,$titel);
+        $stmt->bind_result($advertentieID,$prijs,$titel,$image1path);
         while($stmt->fetch()){
-          echo "<div class='advertentie'></div>";
+          echo "<a src='advertentie.php'><div class='advertentie'><img src='$image1path' /><h6> €".$prijs."</h6><p>".$titel."</p></div></a>";
         }
       }
-      if ($_POST['categorieID'] == 0){
-        $stmt=$con->prepare('SELECT advertentieID,prijs,titel FROM advertenties WHERE title = ? OR beschrijving = ? ORDER BY datumplaatsing DESC, tijdplaatsing DESC LIMIT 10');
-        $stmt->bind_param("ss",$_POST['searchInput'],$_POST['searchInput']);
+      if ($_GET['categorieID'] == 0){
+        $stmt = $con->prepare('SELECT advertentieID,prijs,titel,image1 FROM advertentie WHERE titel LIKE ? OR beschrijving LIKE ? ORDER BY datumplaatsing DESC, tijdplaatsing DESC LIMIT 10');
+        $param = "%".$_GET['searchInput']."%";
+        $stmt->bind_param("ss",$param,$param);
         postAdvertenties($stmt);
       }else{
-        $stmt=$con->prepare('SELECT advertentieID,prijs,titel FROM advertenties WHERE title = ? OR beschrijving = ? AND categorieID = ? ORDER BY datumplaatsing DESC, tijdplaatsing DESC LIMIT 10');
-        $stmt->bind_param("ssi",$_POST['searchInput'],$_POST['searchInput'],$_POST['categorieID']);
+        $stmt = $con->prepare('SELECT advertentieID,prijs,titel,image1 FROM advertentie WHERE titel LIKE ? OR beschrijving LIKE ? AND categorieID = ? ORDER BY datumplaatsing DESC, tijdplaatsing DESC LIMIT 10');
+        $param = "%".$_GET['searchInput']."%";
+        $stmt->bind_param("ssi",$param,$param,$_GET['categorieID']);
         postAdvertenties($stmt);
       }
 
@@ -51,3 +53,4 @@ require_once('includes/get.categorieen.php');
 </div>
 <?php
 require_once('includes/mainend.php');
+
