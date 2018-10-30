0fc737d4f2122004de880b3e5ba72e5751a28934
SanderBuruma <SanderBuruma@gmail.com>
2018-10-30 13:27:49 +0100

    implementing search function

--- ultramarkt/index.php
+++ ultramarkt/index.php
@@ -7,7 +7,10 @@ require_once('includes/navbar.php');
   <form action="index.php">
     <p class="success"><?php echo $_SESSION['success']; $_SESSION['success'] = '' ?></p>
     <p class="warning"><?php echo $_SESSION['warning']; $_SESSION['warning'] = '' ?></p>
-    <input type="text" name="searchInput" id="search-input" placeholder="typ hier uw zoekopdracht!">
+    <div id="search-input" >
+      <input type="text" name="searchInput" placeholder="typ hier uw zoekopdracht!">
+      <select name="categorie"></select>
+    </div>
   </form>
 </div>
 <?php
