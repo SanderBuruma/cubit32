c9aabba3af694693bfe6902833626130011ec313
SanderBuruma <SanderBuruma@gmail.com>
2018-11-01 15:58:57 +0100

    telefoonNr fix

--- ultramarkt/registreren.php
+++ ultramarkt/registreren.php
@@ -5,7 +5,7 @@ if (isset($_POST['submit'])){
 
   $username = filter_var(strtolower($_POST['username']),FILTER_SANITIZE_STRING);
   $email = filter_var(strtolower($_POST['email']),FILTER_SANITIZE_EMAIL);
-  $username = filter_var($_POST['telefoonNr'],FILTER_SANITIZE_STRING);
+  $telefoonNr = filter_var($_POST['telefoonNr'],FILTER_SANITIZE_STRING);
   $passwordConfirm = $_POST['passwordConfirm'];
   $password = $_POST['password'];
 
@@ -35,9 +35,25 @@ if (isset($_POST['submit'])){
 
     $_SESSION['warning'] = "Email al in gebruik";
 
-	} elseif (empty($password) || empty($telefoonNr) || empty($passwordConfirm) || empty($username) || empty($email)){
+	} elseif (empty($password)){
 
-    $_SESSION['warning'] = "Leeg veld";
+    $_SESSION['warning'] = "geen paswoord";
+
+	} elseif (empty($telefoonNr)){
+
+    $_SESSION['warning'] = "geen telefoonnr";
+
+	} elseif (empty($passwordConfirm)){
+
+    $_SESSION['warning'] = "geen herhaald paswoord";
+
+	} elseif (empty($username)){
+
+    $_SESSION['warning'] = "geen gebruikersnaam";
+
+	} elseif (empty($email)){
+
+    $_SESSION['warning'] = "geen E-Mail";
 
 	} elseif (!preg_match("/^[a-zA-Z0-9]*$/",$username)){
 
@@ -81,9 +97,9 @@ require_once('includes/navbar.php');
   <h3>Registratie</h3>
   <p class="success"><?php echo $_SESSION['success']; $_SESSION['success'] = '' ?></p>
   <p class="warning"><?php echo $_SESSION['warning']; $_SESSION['warning'] = '' ?></p>
-  <input type="text" name="username" pattern="[A-Za-z0-9 \-\_]{6,}" oninvalid="setCustomValidity('minstens 6 tekens en alleen letters, nummers, `-` en `_`')" placeholder="Gebruikersnaam"><br/>
+  <input type="text" name="username" pattern="[A-Za-z0-9 \-\_]{6,}" oninvalid="setCustomValidity('minstens 6 tekens en alleen letters, nummers, `-` en `_`')" placeholder="gebruiker"><br/>
   <input type="text" name="email" pattern="^([0-9a-zA-Z]([-\.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$"  oninvalid="setCustomValidity('ongeldige e-mail')" placeholder="E-Mail"><br/>
-  <input type="text" name="telefoonNr" pattern="\d{2,5}-\d{4,15}"  oninvalid="setCustomValidity('alleen nrs en \'-\' toegestaan)" placeholder="06-12345678"><br/>
+  <input type="text" name="telefoonNr" pattern="\d{2,5}-\d{4,15}"  oninvalid="setCustomValidity('alleen nrs en \'-\' toegestaan')" placeholder="telefoon nummer ex. 06-12345678"><br/>
   <input type="password" pattern="((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,})" oninvalid="setCustomValidity('minstens 1 kleine letter, 1 hoofdletter, 1 getal en meer dan 7 tekens')" name="password" value="" placeholder="paswoord"><br/>
   <input type="password" pattern="((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,})" oninvalid="setCustomValidity('minstens 1 kleine letter, 1 hoofdletter, 1 getal en meer dan 7 tekens')" name="passwordConfirm" placeholder="zelfde paswoord"><br/>
   <button type="submit" name='submit'>Registreer!</button>
