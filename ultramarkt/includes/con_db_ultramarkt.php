994beb11905b88afa4108b22cf623943a884a310
SanderBuruma <SanderBuruma@gmail.com>
2018-11-02 21:59:34 +0100

    fixing password?

--- ultramarkt/includes/con_db_ultramarkt.php
+++ ultramarkt/includes/con_db_ultramarkt.php
@@ -1,6 +1,6 @@
 <?php
 $address = 'localhost';
 $user = 'root';
-$password = 'w34#9^lgBJKV';
+$password = $PHPMARootPassword;
 $database = 'ultramarkt';
 $con = new mysqli("$address", "$user", "$password", "$database");
--- ultramarkt/includes/main.php
+++ ultramarkt/includes/main.php
@@ -11,16 +11,14 @@ if (!isset($_SESSION['warning'])){
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-//grab special svg font characters 
-require_once("svg.php");
-
-//iniate connection to db
-require_once('includes/con_db_ultramarkt.php');
-
 //everyting else
 require_once('../../hidden/passwords.php');
 $PHPMARootPassword = $PHPMAPasswords['root'];
+//iniate connection to db
+require_once('includes/con_db_ultramarkt.php');
 require_once('includes/class.userInterface.php');
+//grab special svg font characters 
+require_once("svg.php");
 
 //initiate reference arrays
 $alphaNumericChars = str_split("abcdefghijklmnopqrstuvwxyzABCDEFGHIJVKLMNOPQRSTUVWXYZ1234567890");
