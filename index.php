8e9eccac180a7f4d2888fe50a88a6a7a820352fb
SanderBuruma <sanderburuma@gmail.com>
2018-11-25 22:03:20 +0100

    include hotjar tracking code

--- index.php
+++ index.php
@@ -153,5 +153,17 @@
     </div>
   </div>
   <!-- <script defer type="text/javascript" src="main.js"></script> -->
+
+  <!-- hotjar.com -->
+  <script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1103423,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 </body>
 </html>
